8cdcd3ebb39b1c5ea62019e5d86728a9f7d262a4
ACatfromUSTC <wzh1997@mail.ustc.edu.cn>
2020-11-23 21:18:55 +0800

    Update Predict.py

--- EPBoost/Predict.py
+++ EPBoost/Predict.py
@@ -32,8 +32,8 @@ from scipy.stats import pearsonr
 import matplotlib.pyplot as plt
 
 kvalue = int(sys.argv[1])
-model_file = '/EPBoost/EPBoost/dataset/'
-model_select = 'TargetFinder/'
+model_file = '/bio_data/wangzihang/percentkequals3/kequals3/EPnet6'
+model_select = '/'
 cellline = str(sys.argv[2])
 enchrome = str(sys.argv[3])
 enstart = str(sys.argv[4])
@@ -46,10 +46,12 @@ newenstart = enmid - 1500
 newenend = newenstart + 2999
 prmid = (int(prstart)+int(prend))//2
 newprstart = prmid-1000
-newprend = newenstart + 1999
-distance = abs(prend - enmid)
+newprend = newprstart + 1999
+distance = abs(prmid-enmid)
 dis =  '%.4f' % math.log((2000000/distance),10)
-model_filepath = model_file+model_select+cellline
+model_filepath = model_file+model_select
+enoldname = enchrome+':'+str(enstart)+'-'+str(enend)
+proldname = prchrome+':'+str(prstart)+'-'+str(prend)
 enname = enchrome+':'+str(newenstart)+'-'+str(newenend)
 prname = prchrome+':'+str(newprstart)+'-'+str(newprend)
 
@@ -61,15 +63,16 @@ train_num = 1
 
 fin1 = open('enhancer.bed','w')
 fin2 = open('promoter.bed','w')
-fin1.write(enchrome+'\t'+str(newenstart)+'\t'+str(newenend)+'\t'+enname+'\n')
-fin2.write(prchrome+'\t'+str(newprstart)+'\t'+str(newprend)+'\t'+prname+'\n')
+for i in range(2):
+    fin1.write(enchrome+'\t'+str(newenstart)+'\t'+str(newenend)+'\t'+enname+'\n')
+    fin2.write(prchrome+'\t'+str(newprstart)+'\t'+str(newprend)+'\t'+prname+'\n')
 fin1.close()
 fin2.close()
 
-os.system("bedtools getfasta -fi ../hg19.fa -bed enhancer.bed -fo enhancer.fa")
-os.system("bedtools getfasta -fi ../hg19.fa -bed promoter.bed -fo promoter.fa")
-os.system("python3 ../seekr_py/src/kmer_counts.py enhancer.fa -o enhancer.txt -k {} -nb")
-os.system("python3 ../seekr_py/src/kmer_counts.py promoter.fa -o promoter.txt -k {} -nb")
+os.system("bedtools getfasta -fi /bio_data/wangzihang/epnet/hg19.fa -bed enhancer.bed -fo enhancer.fa")
+os.system("bedtools getfasta -fi /bio_data/wangzihang/epnet/hg19.fa -bed promoter.bed -fo promoter.fa")
+os.system("python3 /bio_data/wangzihang/percentkequals3/seekr_py/src/kmer_counts.py enhancer.fa -o enhancer.txt -k {} -nb".format(kvalue))
+os.system("python3 /bio_data/wangzihang/percentkequals3/seekr_py/src/kmer_counts.py promoter.fa -o promoter.txt -k {} -nb".format(kvalue))
 
 
 
@@ -97,8 +100,8 @@ for n,line in enumerate(fin2):
 promoter = df2
 
 
-enhancer_vec = enhancer[int(data[0])]
-promoter_vec = promoter[int(data[1])]
+enhancer_vec = enhancer[0]
+promoter_vec = promoter[0]
 enhancer_vec = enhancer_vec.reshape((1,kmer))
 promoter_vec = promoter_vec.reshape((1,kmer))
 arrays[0] = numpy.column_stack((enhancer_vec,promoter_vec))
@@ -109,12 +112,20 @@ distance[0] = float(dis)
 
 
 X_train = numpy.column_stack((arrays,distance))
-print(X_train.shape[0])
+print(X_train.shape[0],X_train.shape[1])
 
 
 
 estimator = CatBoostClassifier(iterations = 1000,depth = 10,learning_rate = 0.1,logging_level = None,scale_pos_weight = 45)
-estimator.load_model('{}/best_model3'.format(model_filepath))
+estimator.load_model('{}/best_model{}'.format(model_filepath,kvalue))
 
-y_pred = estimator.predict(X_train[test,:])
-y_proba_pred = estimator.predict_proba(X_train[test,:])[:,1]
+y_pred = estimator.predict(X_train)
+y_proba_pred = estimator.predict_proba(X_train)[:,1]
+if enchrome != prchrome:
+    print('The two elements are not in the same chrosome, please recheck your input')
+else:
+    print(enoldname,proldname)
+    if y_pred[0] == 0:
+        print('The two elements are predicted not to be interacted by EPBoost, the interaction prediction score is %.4f'%y_proba_pred[0])
+    else:
+        print('The two elements are predicted interacted by EPBoost, the interaction prediction score is %.4f'%y_proba_pred[0])
