4b52aa1af546558a105c87bd8f3fb76da5a5b5d5
ACatfromUSTC <wzh1997@mail.ustc.edu.cn>
2020-12-10 19:56:16 +0800

    Update EPBoost2_Train.py

--- EPBoost/dataset/TargetFinder/NHEK/EPBoost2_Train.py
+++ EPBoost/dataset/TargetFinder/NHEK/EPBoost2_Train.py
@@ -70,9 +70,9 @@ for line in fin3:
 		continue
 	else:
 		data = line.strip().split(',')
-		enhancer_index = enhancers.index(data[4])
-		promoter_index = promoters.index(data[8])
-		fout1.write(str(enhancer_index)+'\t'+str(promoter_index)+'\t'+data[9]+'\t'+data[10]+'\n')
+		enhancer_index = enhancers.index(data[5])
+		promoter_index = promoters.index(data[9])
+		fout1.write(str(enhancer_index)+'\t'+str(promoter_index)+'\t'+data[10]+'\t'+data[11]+'\n')
 		train_num = train_num + 1
 fout1.close()
 print(train_num)
